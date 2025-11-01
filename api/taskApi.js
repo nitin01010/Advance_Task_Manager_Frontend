9b332de6c2b6b4168c9fc56e88f1663a5c3fa699
Nitin0202 <nitinsachdeva0202@gmail.com>
2025-11-01 14:21:45 +0530

    updates basic url

--- api/taskApi.js
+++ api/taskApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export async function handleGETAllTask() {
   try {
-    const response = await axios.get("http://localhost:3000/api/v1/task");
+    const response = await axios.get("https://advance-task-manager-backend.onrender.com/api/v1/task");
     return response.data;
   } catch (error) {
     throw new Error(error.response?.data?.message || "Failed to fetch tasks");
@@ -10,21 +10,21 @@ export async function handleGETAllTask() {
 }
 
 export async function handleCreateTask(taskData) {
-  const response = await axios.post("http://localhost:3000/api/v1/task", { title: taskData.task });
+  const response = await axios.post("https://advance-task-manager-backend.onrender.com/api/v1/task", { title: taskData.task });
   return response.data;
 }
 
 export async function handleDeleteTask(taskId) {
-  const response = await axios.delete(`http://localhost:3000/api/v1/task/${taskId}`);
+  const response = await axios.delete(`https://advance-task-manager-backend.onrender.com/api/v1/task/${taskId}`);
   return response.data;
 }
 
 export async function handleUpdateTask({ id, title }) {
-  const response = await axios.put(`http://localhost:3000/api/v1/task/${id}`, { title });
+  const response = await axios.put(`https://advance-task-manager-backend.onrender.com/api/v1/task/${id}`, { title });
   return response.data;
 }
 
 export async function toggleTaskStatus(id) {
-  const response = await axios.patch(`http://localhost:3000/api/v1/task/${id}/toggle`);
+  const response = await axios.patch(`https://advance-task-manager-backend.onrender.com/api/v1/task/${id}/toggle`);
   return response.data;
 }
